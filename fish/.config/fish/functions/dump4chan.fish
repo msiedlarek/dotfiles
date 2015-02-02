ccf09276b13c4d8277a21c1942133bc831feb152
Mikołaj Siedlarek <mikolaj@siedlarek.pl>
2015-02-02 19:49:36 +0100

    fixed certificate problem with dump4chan

--- fish/.config/fish/functions/dump4chan.fish
+++ fish/.config/fish/functions/dump4chan.fish
@@ -1,3 +1,3 @@
 function dump4chan --description 'Downloads images from given 4chan thread url'
-    wget -O - $argv | grep -Eo 'i.4cdn.org/[^"]+' | sed 's/^/https:\/\//' | sort | uniq | xargs wget --no-clobber
+    wget --no-check-certificate -O - $argv | grep -Eo 'i.4cdn.org/[^"]+' | sed 's/^/https:\/\//' | sort | uniq | xargs wget --no-check-certificate --no-clobber
 end
