21bc48e0e7b06bdc1ba735aeb324f782acb9af24
Mikołaj Siedlarek <mikolaj@siedlarek.pl>
2015-12-21 13:55:20 +0100

    Use GitHub style for Markdown preview in Atom.

--- fish/.config/fish/functions/dump4chan.fish
+++ fish/.config/fish/functions/dump4chan.fish
@@ -1,3 +1,3 @@
 function dump4chan --description 'Downloads images from given 4chan thread url'
-    wget --no-check-certificate -O - $argv | grep -Eo 'i.4cdn.org/[^"]+' | sed 's/^/https:\/\//' | grep -v 's\.' | sort | uniq | xargs wget --no-check-certificate --no-clobber
+    wget --directory-prefix=. --span-hosts --no-directories --recursive --execute="robots=off" --domains=i.4cdn.org --accept="*.jpg,*.png,*.gif,*.webm" --reject="*s.jpg" --no-clobber $argv
 end
