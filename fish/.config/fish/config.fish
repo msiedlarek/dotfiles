e027dc98c0565710b6ce3f6c2648f7dda9a9d126
Mikołaj Siedlarek <mikolaj@siedlarek.pl>
2016-03-14 20:38:44 +0100

    Fish configuration cleanup.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,18 +3,26 @@
     set -x LC_ALL en_US.UTF-8
     set -x LANG en_US.UTF-8
 
-    # Use Vim as default editor
-    set -x EDITOR vim
-    set -x VISUAL $EDITOR
+    # Prefer NeoVim or Vim as default editor
+    if type -fq nvim
+        set -x EDITOR nvim
+        alias vim nvim
+    else if type -fq vim
+        set -x EDITOR vim
+    end
 
-    # Use less without clearing the screen.
+    if test -n $EDITOR
+        set -x VISUAL $EDITOR
+    end
+
+    # Do not clear the screen after quitting man or less.
     set -x PAGER 'less -X'
     set -x MANPAGER 'less -X'
 
-    # Enable colored grep output
+    # Enable colored grep output.
     set -x GREP_OPTIONS '--color=auto'
 
-    # Set PATH
+    # Add /usr/local/bin to PATH.
     set -x PATH '/usr/local/bin' $PATH
 
     # Run private scripts that cannot be released in public dotfiles
@@ -23,10 +31,12 @@
         source $script
     end
 
-    # Prefer Java 8
+    # Prefer Java 8.
     if test -x '/usr/libexec/java_home'
         set -x JAVA_HOME (/usr/libexec/java_home -v 1.8 ^/dev/null)
     end
 
+    # Setup Go environment.
     set -x GOPATH "$HOME/Projects/go"
+    set -x PATH "$GOPATH/bin" $PATH
 # end
