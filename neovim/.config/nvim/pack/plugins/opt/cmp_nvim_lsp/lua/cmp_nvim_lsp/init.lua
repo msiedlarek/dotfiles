dcd2b0092d62e34e57dc624666b389e57f3a32f9
Mikołaj Siedlarek <mikolaj@siedlarek.net>
2022-10-17 21:08:13 +0200

    git subrepo pull neovim/.config/nvim/pack/plugins/opt/cmp_nvim_lsp
    subrepo:
      subdir:   "neovim/.config/nvim/pack/plugins/opt/cmp_nvim_lsp"
      merged:   "3cf38d9c"
    upstream:
      origin:   "https://github.com/hrsh7th/cmp-nvim-lsp.git"
      branch:   "main"
      commit:   "3cf38d9c"
    git-subrepo:
      version:  "0.4.5"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "aa416e4"

--- neovim/.config/nvim/pack/plugins/opt/cmp_nvim_lsp/lua/cmp_nvim_lsp/init.lua
+++ neovim/.config/nvim/pack/plugins/opt/cmp_nvim_lsp/lua/cmp_nvim_lsp/init.lua
@@ -20,28 +20,55 @@ local if_nil = function(val, default)
   return val
 end
 
-M.update_capabilities = function(capabilities, override)
+-- Backported from vim.deprecate (0.9.0+)
+local function deprecate(name, alternative, version, plugin, backtrace)
+  local message = name .. ' is deprecated'
+  plugin = plugin or 'Nvim'
+  message = alternative and (message .. ', use ' .. alternative .. ' instead.') or message
+  message = message
+    .. ' See :h deprecated\nThis function will be removed in '
+    .. plugin
+    .. ' version '
+    .. version
+  if vim.notify_once(message, vim.log.levels.WARN) and backtrace ~= false then
+    vim.notify(debug.traceback('', 2):sub(2), vim.log.levels.WARN)
+  end
+end
+
+M.default_capabilities = function(override)
   override = override or {}
 
-  local completionItem = capabilities.textDocument.completion.completionItem
-
-  completionItem.snippetSupport = if_nil(override.snippetSupport, true)
-  completionItem.preselectSupport = if_nil(override.preselectSupport, true)
-  completionItem.insertReplaceSupport = if_nil(override.insertReplaceSupport, true)
-  completionItem.labelDetailsSupport = if_nil(override.labelDetailsSupport, true)
-  completionItem.deprecatedSupport = if_nil(override.deprecatedSupport, true)
-  completionItem.commitCharactersSupport = if_nil(override.commitCharactersSupport, true)
-  completionItem.tagSupport = if_nil(override.tagSupport, { valueSet = { 1 } })
-  completionItem.resolveSupport = if_nil(override.resolveSupport, {
-    properties = {
-      'documentation',
-      'detail',
-      'additionalTextEdits',
-    }
-  })
-
-  return capabilities
+  return {
+    textDocument = {
+      completion = {
+        completionItem = {
+          snippetSupport = if_nil(override.snippetSupport, true),
+          preselectSupport = if_nil(override.preselectSupport, true),
+          insertReplaceSupport = if_nil(override.insertReplaceSupport, true),
+          labelDetailsSupport = if_nil(override.labelDetailsSupport, true),
+          deprecatedSupport = if_nil(override.deprecatedSupport, true),
+          commitCharactersSupport = if_nil(override.commitCharactersSupport, true),
+          tagSupport = if_nil(override.tagSupport, { valueSet = { 1 } }),
+          resolveSupport = if_nil(override.resolveSupport, {
+              properties = {
+                  "documentation",
+                  "detail",
+                  "additionalTextEdits",
+              },
+          }),
+        }
+      },
+    },
+  }
 end
+
+---Backwards compatibility
+M.update_capabilities = function(capabilities, override)
+  local _deprecate = vim.deprecate or deprecate
+  _deprecate('cmp_nvim_lsp.update_capabilities', 'cmp_nvim_lsp.default_capabilities', '1.0.0', 'cmp-nvim-lsp')
+  return M.default_capabilities(override)
+end
+
 
 ---Refresh sources on InsertEnter.
 M._on_insert_enter = function()
@@ -81,4 +108,3 @@ M._on_insert_enter = function()
 end
 
 return M
-
