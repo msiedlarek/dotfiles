9d93c56114bbfdb9a8bc0169b4a5b8b311889126
Mikołaj Siedlarek <mikolaj@siedlarek.net>
2022-10-17 21:46:58 +0200

    Remove old zinit install script.

--- zsh/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ ! -d ~/.zinit/bin ]]; then
-  mkdir -p ~/.zinit
-  git clone --depth 1 -b v3.7 https://github.com/zdharma-continuum/zinit.git ~/.zinit/bin
-  zsh -c 'source ~/.zshrc && zinit self-update'
-fi
